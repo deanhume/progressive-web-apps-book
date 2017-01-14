e9c61aec7ad3f7a1e3e8107c77b35edecec3b42c
Dean <contact@deanhume.com>
2017-01-14 10:00:03 +0000

    Updated to use web-push

--- chapter-6/push-notifications/server.js
+++ chapter-6/push-notifications/server.js
@@ -1,40 +1,62 @@
 const webpush = require('web-push');
-const express = require('express')
+const express = require('express');
+var bodyParser = require('body-parser');
 const app = express();
 
-// VAPID keys should only be generated only once. 
-const vapidKeys = webpush.generateVAPIDKeys();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+// Set CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+// VAPID keys should only be generated only once.
+//const vapidKeys = webpush.generateVAPIDKeys();
 
 webpush.setVapidDetails(
   'mailto:contact@deanhume.com',
-  vapidKeys.publicKey,
-  vapidKeys.privateKey
+  'BAyb_WgaR0L0pODaR7wWkxJi__tWbM1MPBymyRDFEGjtDCWeRYS9EF7yGoCHLdHJi6hikYdg4MuYaK0XoD0qnoY',
+  'p6YVD7t8HkABoez1CvVJ5bl7BnEdKUu5bSyVjyxMBh0'
 );
 
-// Send a message
-app.get('/sendMessage', function (req, res) {
-  // This is the same output of calling JSON.stringify on a PushSubscription 
-  // This information comes from the server
-  const pushSubscription = {
-    endpoint: '.....',
-    keys: {
-      auth: '.....',
-      p256dh: '.....'
-    }
-  };
+app.get('/', function (req, res) {
+  res.send('success');
+});
 
-  webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
+// Send a message
+app.post('/sendMessage', function (req, res) {
+  // This is the same output of calling JSON.stringify on a PushSubscription
+  // This information comes from the server
+
 });
 
 // Register the user
-app.get('/register', function (req, res) {
+app.post('/register', function (req, res) {
+
+  var endpoint = req.body.endpoint;
+
+  const pushSubscription = {
+    endpoint: req.body.endpoint,
+    keys: {
+      auth: req.body.authSecret,
+      p256dh: req.body.key
+    }
+  };
+
+  webpush.sendNotification(pushSubscription, 'Thank you for registering')
+          .then(result => res.sendStatus(201))
+          .catch(err => {
+            console.log(err);  
+          });
 
 });
 
 // The server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(3111, function () {
+  console.log('Example app listening on port 3111!')
 });
-
-
-

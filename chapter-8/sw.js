945f026d8ec4b13d3a8bf00798d8dc2895e861f5
deanhume <deanhume@gmail.com>
2017-03-15 06:04:41 +0000

    Updated swer with final code

--- chapter-8/sw.js
+++ chapter-8/sw.js
@@ -5,17 +5,17 @@ const offlineUrl = 'offline-page.html';
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName)
-      .then(cache => cache.addAll([
-        './js/main.js',
-        './js/article.js',
-        './images/newspaper.svg',
-        './css/site.css',
-        './data/latest.json',
-        './data/data-1.json',
-        './article.html',
-        './index.html',
-        offlineUrl
-      ]))
+    .then(cache => cache.addAll([
+      './js/main.js',
+      './js/article.js',
+      './images/newspaper.svg',
+      './css/site.css',
+      './data/latest.json',
+      './data/data-1.json',
+      './article.html',
+      './index.html',
+      offlineUrl
+    ]))
   );
 });
 
@@ -38,7 +38,7 @@ function resolveFirstPromise(promises) {
     promises.forEach(p => p.then(resolve));
 
     promises.reduce((a, b) => a.catch(() => b))
-      .catch(() => reject(Error("All failed")));
+    .catch(() => reject(Error("All failed")));
   });
 };
 
@@ -54,41 +54,37 @@ self.addEventListener('fetch', function(event) {
       ])
     );
   }
-});
 
-// Cache any new resources as they are fetched
-// self.addEventListener('fetch', function(event) {
-//
-//     event.respondWith(
-//       caches.match(event.request)
-//       .then(function(response) {
-//         if (response) {
-//           return response;
-//         }
-//         var fetchRequest = event.request.clone();
-//
-//         return fetch(fetchRequest).then(
-//           function(response) {
-//             if(!response || response.status !== 200) {
-//               return response;
-//             }
-//
-//             var responseToCache = response.clone();
-//             caches.open(cacheName)
-//             .then(function(cache) {
-//               cache.put(event.request, responseToCache);
-//             });
-//
-//             return response;
-//           }
-//         ).catch(error => {
-//           // Check if the user is offline first and is trying to navigate to a web page
-//           if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
-//           // Return the offline page
-//           return caches.match(offlineUrl);
-//         }
-//         });
-//       })
-//     );
-//
-// });
+  // Else process all other requests as expected
+  event.respondWith(
+    caches.match(event.request)
+    .then(function(response) {
+      if (response) {
+        return response;
+      }
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(
+        function(response) {
+          if(!response || response.status !== 200) {
+            return response;
+          }
+
+          var responseToCache = response.clone();
+          caches.open(cacheName)
+          .then(function(cache) {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        }
+      ).catch(error => {
+        // Check if the user is offline first and is trying to navigate to a web page
+        if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
+          // Return the offline page
+          return caches.match(offlineUrl);
+        }
+      });
+    })
+  );
+});

a39a663a710f02e27c6d2f075376b690454fcc61
deanhume <dean@settled.co.uk>
2017-04-03 17:38:11 +0100

    Exploring streams

---
+++ chapter-10/sw.js
@@ -0,0 +1,142 @@
+const cacheName = 'latestNews-v1';
+const offlineUrl = 'offline-page.html';
+
+// Cache our known resources during install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        './js/main.js',
+        './js/article.js',
+        './images/newspaper.svg',
+        './css/site.css',
+        // './data/latest.json',
+        // './data/data-1.json',
+        './article.html',
+        './index.html',
+        offlineUrl
+      ]))
+  );
+});
+
+function timeout(delay) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      resolve(new Response('', {
+        status: 408,
+        statusText: 'Request timed out.'
+      }));
+    }, delay);
+  });
+}
+
+function resolveFirstPromise(promises) {
+  return new Promise((resolve, reject) => {
+
+    promises = promises.map(p => Promise.resolve(p));
+
+    promises.forEach(p => p.then(resolve));
+
+    promises.reduce((a, b) => a.catch(() => b))
+      .catch(() => reject(Error("All failed")));
+  });
+};
+
+function streamArticle() {
+  try {
+    new ReadableStream({});
+  }
+  catch (e) {
+    return new Response("Streams not supported");
+  }
+  const stream = new ReadableStream({
+    start(controller) {
+      // const contentURL = new URL(url);
+      // contentURL.pathname += '.middle.inc';
+      const startFetch = caches.match('./article-header.html');
+      const bodyData = fetch('./data/data-1.html').catch(() => new Response("Failed, soz"));
+      const endFetch = caches.match('./article-footer.html');
+
+      function pushStream(stream) {
+        const reader = stream.getReader();
+        function read() {
+          return reader.read().then(result => {
+            if (result.done) return;
+            controller.enqueue(result.value);
+            return read();
+          });
+        }
+        return read();
+      }
+
+      startFetch
+        .then(response => {
+          console.log(response.body);
+          return pushStream(response.body)}
+          )
+        .then(() => bodyData)
+        .then(response => pushStream(response.body))
+        .then(() => endFetch)
+        .then(endFetch => pushStream(response.body))
+        .then(() => controller.close());
+    }
+  });
+
+  return new Response(stream, {
+    headers: { 'Content-Type': 'application/json' }
+  })
+}
+
+
+self.addEventListener('fetch', function (event) {
+
+  // Check for the json file
+  if (/article.html/.test(event.request.url)) {
+    event.respondWith(streamArticle());
+  }
+
+  // Check for the googleapis domain
+  // if (/googleapis/.test(event.request.url)) {
+  //   event.respondWith(
+  //     resolveFirstPromise([
+  //       timeout(500),
+  //       fetch(event.request)
+  //     ])
+  //   );
+  // }
+
+  // Else process all other requests as expected
+  // event.respondWith(
+  //   caches.match(event.request)
+  //   .then(function(response) {
+  //     if (response) {
+  //       return response;
+  //     }
+  //     var fetchRequest = event.request.clone();
+
+  //     return fetch(fetchRequest).then(
+  //       function(response) {
+  //         if(!response || response.status !== 200) {
+  //           return response;
+  //         }
+
+  //         var responseToCache = response.clone();
+  //         caches.open(cacheName)
+  //         .then(function(cache) {
+  //           cache.put(event.request, responseToCache);
+  //         });
+
+  //         return response;
+  //       }
+  //     ).catch(error => {
+  //       // Check if the user is offline first and is trying to navigate to a web page
+  //       if (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html')) {
+  //         // Return the offline page
+  //         return caches.match(offlineUrl);
+  //       }
+  //     });
+  //   })
+  // );
+});
+
+//https://jakearchibald.github.io/isserviceworkerready/demos/simple-stream/sw.js
